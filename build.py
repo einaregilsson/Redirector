3ba13e5bcb4177abc3aaef2da37c2b99befd6c88
Einar Egilsson <einar@einaregilsson.com>
2011-08-27 08:29:54 +0200

    Removed unittests from build

--- build.py
+++ build.py
@@ -2,6 +2,8 @@ import os, os.path, zipfile
 
 xpi = zipfile.ZipFile('redirector-2.6.xpi','w')
 for (root, folders, files) in os.walk('.'):
+	if 'unittest' in root:
+		continue
 	if not '.svn' in root:
 		for f in files:
 			if f.lower().endswith(('.rdf', '.manifest', '.js', '.xpt', '.png', '.css', '.dtd', '.properties', '.xul', '.html')):
