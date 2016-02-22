4cd86af900055192fabb0d08aa90cc0c067a4fa5
Einar Egilsson <einar@einaregilsson.com>
2016-02-22 10:14:31 +0000

    Show message in Firefox private mode that Redirector does not work

--- js/redirector-scriptload.js
+++ js/redirector-scriptload.js
@@ -17,3 +17,8 @@ if (typeof chrome !== 'undefined') {
 	loadScript("js/controllers/importexport.js");
 	loadScript("js/controllers/listredirects.js");
 }
+
+//To make the private stuff in Firefox work properly
+window.addEventListener('DOMContentLoaded', function() {
+	document.body.classList.remove('private');
+});
